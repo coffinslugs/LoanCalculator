3bd04265b1bab610e932440fff14bc0bc0cdd906
jcodesthings <55113474+jcodesthings@users.noreply.github.com>
2022-01-03 09:52:23 -0700

    Models Created
    Loan and LoanPayment models have been created.

---
+++ LoanCalculator/Models/Loan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoanCalculator.Models
+{
+    public class Loan
+    {
+        public decimal FullAmount { get; set; }
+        public decimal InterestRate { get; set; }
+        public int TermInMonths { get; set; }
+        public decimal MonthlyPayment { get; set; }
+        public decimal TotalInterest { get; set; }
+        public decimal TotalCost { get; set; }
+        public List<LoanPayment> Payments { get; set; } = new List<LoanPayment>();
+    }
+}
